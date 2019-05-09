e3bf2cc4288b0aa167570e74a637a08d753fba8b
Seonghyeon Choe <kkabdol@gmail.com>
2019-05-10 00:40:11 +0900

    Premake setup

---
+++ premake5.lua
@@ -0,0 +1,57 @@
+project "glfw"
+    kind "StaticLib"
+    language "C"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "include/glfw/glfw3.h",
+        "include/glfw/glfw3native.h",
+        "src/glfw_config.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/vulkan.c",
+        "src/window.c"
+    }
+
+    filter "system:windows"
+        cdialect "C11"
+        staticruntime "on"
+        systemversion "latest"
+
+        files                       
+        {
+            "src/win32_platform.h",
+            "src/win32_joystick.h",
+            "src/wgl_context.h",
+            "src/egl_context.h",
+            "src/osmesa_context.h",
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
+
+        defines
+        {
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
+
+        filter { "system:windows", "configurations:Debug" }
+            symbols "On"
+
+        filter { "system:windows", "configurations:Release" }
+            optimize "On"
+        
+        filter { "system:windows", "configurations:Distribution" }
+            optimize "On"
