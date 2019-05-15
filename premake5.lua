54f2030f1dd6021b44d42b4795033847aa722c29
Seonghyeon Choe <kkabdol@gmail.com>
2019-05-15 22:14:02 +0900

    Move output destinations.

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "glfw"
     kind "StaticLib"
     language "C"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
     files
     {
